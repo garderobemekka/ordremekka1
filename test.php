1c42d8f47f380db71aa72842e83f32bd4d5d8801
garderobemekka <124746401+garderobemekka@users.noreply.github.com>
2023-02-08 17:19:53 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+
+	$username	= "ordremeekka";   
+	$password	= "Gme1234567890987654321";
+	$dbname		= "gme";
+	$host		= "ordremekka.mysql.database.azure.com";
+  
+  $connection = mysqli_init();
+	   mysqli_ssl_set($connection,NULL,NULL, "/var/www/html/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+             mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+
+
+if (mysqli_connect_errno($connection)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
