47cb69950f5d62718ac78b40f30d04d265578b50
garderobemekka <124746401+garderobemekka@users.noreply.github.com>
2023-02-08 12:55:20 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello SVENNO";
+
+?>
+<html>
+<style>
+body{
+    background-image: url('assets/img/bg-darkmode.jpg');
+
+}
+.bilde{
+    top: 45%;
+    margin-top: 45%;
+}
+
+</style>
+
+<div class="bilde"><center><img src="assets/img/ordremekka_hvit.png" width="500px"></center></div>
+
+
+</html>
